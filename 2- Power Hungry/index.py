3766029ea1b3ec9774f63d645d385915ae54df3b
sedhossein <seyedhosein77@gmail.com>
2020-03-05 18:55:22 +0330

    clean-up time

--- 2- Power Hungry/index.py
+++ 2- Power Hungry/index.py
@@ -1,39 +1,35 @@
 def solution(xs):
     posArray = []
     negArray = []
-    neg2posArray = []
-
-    # detect special single values
-    if len(xs) == 1:
-        if xs[0] > 0:
-            return str(xs[0])
-        else:
-            return "0"
 
     # remove zeros and separate neg/pos digits
     for v in xs:
-        if v <= -1:
+        if v < 0:
             negArray += [v]
-        elif v >= 1:
+        elif v > 0:
             posArray += [v]
 
-    # detect single negative input
-    if len(posArray) == 0 and len(negArray) == 1:
+    # detect empty/singe negative input
+    if len(posArray) == 0 and (len(negArray) == 0 or len(negArray) == 1):
         return "0"
+
+    # detect single positive input
+    if len(posArray) == 1 and len(negArray) == 0:
+        return str(posArray[0])
 
     # considering `neg*neg=pos`
     if len(negArray) % 2 != 0:
-        negArray = sorted(negArray)
-        negArray.pop()
-
-    # convert negative digits to positive
-    for v in negArray:
-        neg2posArray += [v * -1]
+        negArray.remove(max(negArray))
 
     # calculate the power
-    res = "1"
-    array = neg2posArray + posArray
+    res = 1
+    array = negArray + posArray
     for i in array:
-        res = str(int(res) * i)
+        res *= i
 
-    return res
+    return str(res)
+
+
+print(solution([-1, -2, -3, 1]))
+print(solution([-2, -3, 4, -5]))
+print(solution([2, 0, 2, 2, 0]))
