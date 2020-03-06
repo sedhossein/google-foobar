0465ed48b6b9e973b189d7cca4e3d7fb7be255d5
sedhossein <seyedhosein77@gmail.com>
2020-03-06 17:49:50 +0330

    solve another easy challenge

--- 2- Power Hungry/forthWay.py
+++
@@ -1,9 +0,0 @@
-import numpy
-
-
-def solution(xs):
-    temp = list(filter((0).__ne__, xs))
-    result = numpy.prod(temp)
-    if result < 0:
-        result /= max([n for n in temp if n < 0])
-    return str(result)
---
+++ 2-2 Bunny Prisoner Locating/index.py
@@ -0,0 +1,15 @@
+def collectionSum(till):
+    return (till * (1 + till)) / 2
+
+
+def solution(x, y):
+    if x < 1 or y < 1 or x > 100000 or y > 100000:
+        raise ValueError("invalid address!")
+
+    xPrim = collectionSum(x)
+    yPrim = collectionSum(y) - (y - 1)
+
+    return str(xPrim + yPrim + (x - 1)*(y - 1) - 1)
+
+
+print(solution(5, 2))
