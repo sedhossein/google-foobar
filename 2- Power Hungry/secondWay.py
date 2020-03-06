6a1c16dea6bf16e67ce0abad9754c40d48048acb
sedhossein <seyedhosein77@gmail.com>
2020-03-06 15:46:15 +0330

    finally solved

--- 2- Power Hungry/secondWay.py
+++ 2- Power Hungry/secondWay.py
@@ -2,61 +2,30 @@ def solution(xs):
     maxNeg = -9999999
     count = 0
     countNeg = 0
-    res = "1"
+    res = str(int(1))
 
-    for i in xs:
-        if i == 0:
+    if len(xs) > 50 or len(xs) < 1 or max(xs) > 1000 or min(xs) < -1000:
+        raise ValueError('invalid inputs')
+
+    if len(xs) == 1:
+        return str(xs[0])
+
+    for v in xs:
+        if v == 0:
             continue
 
         count += 1
-        if i < 0:
+        if v < 0:
             countNeg += 1
-            if maxNeg < i:
-                maxNeg = i
+            if maxNeg < v:
+                maxNeg = v
 
-        res = str(int(res) * i)
+        res = str(int(res) * v)
 
     if countNeg == 1 and count == 1:
         return "0"
 
     if countNeg % 2 == 1:
-        res = str(int(res) / maxNeg)
+        res = str(int(int(res) / maxNeg))
 
-    return str(res) if count > 0 else "0"
-
-
-print(solution([0]) == "0")
-print(solution([2]) == "2")
-print(solution([-2]) == "0")
-print("===========================")
-print(solution([0, 0]) == "0")
-print(solution([-2, 0]) == "0")
-print(solution([2, 0]) == "2")
-print(solution([-2, 2]) == "2")
-print(solution([-2, -2]) == "4")
-print(solution([2, 2]) == "4")
-print("===========================")
-print(solution([0, 0, 0]) == "0")
-print(solution([0, 0, 2]) == "2")
-print(solution([0, 2, 2]) == "4")
-print(solution([2, 2, 2]) == "8")
-print(solution([-2, -2, -2]) == "4")
-print(solution([0, -2, -2]) == "4")
-print(solution([0, 0, -2]) == "0")
-print(solution([0, 0, 0]) == "0")
-print(solution([2, 2, -2]) == "4")
-print(solution([2, -2, -2]) == "8")
-print("===========================")
-print(solution([0, 0, 0, 0]) == "0")
-print(solution([0, 0, 0, -2]) == "0")
-print(solution([0, 0, -2, -2]) == "4")
-print(solution([0, -2, -2, -2]) == "4")
-print(solution([-2, -2, -2, -2]) == "16")
-print(solution([2, 2, 2, 2]) == "16")
-print(solution([2, 2, 2, 0]) == "8")
-print(solution([2, 2, 0, 0]) == "4")
-print(solution([2, 0, 0, 0]) == "2")
-print(solution([2, -2, 2, -2]) == "16")
-print(solution([2, -2, 0, -2]) == "8")
-print(solution([2, 2, 0, -2]) == "4")
-print(solution([2, -2, 0, 0]) == "2")
+    return res if count > 0 else "0"
